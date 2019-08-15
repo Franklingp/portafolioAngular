abd950592684cb416e842f1d90b12389bb6825cb
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2019-08-14 20:14:03 -0500

    Se ha agregado el el modulo app y la conexion con la base de datos del backend

---
+++ server/app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// Modulo de configuracion del backend
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//Importacion de rutas
+
+//Middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//Rutas
+
+//Exportar
+module.exports = app;
